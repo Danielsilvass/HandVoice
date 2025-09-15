02e7fcab32120ca91c5d14bf00c826fb9aeb5258
danielssilvas123 <danielssilvas123@gmail.com>
2025-09-15 11:54:13 +0000

    Reference (Links)

--- HandVoice/src/pages/Reference.jsx
+++ HandVoice/src/pages/Reference.jsx
@@ -7,10 +7,10 @@ export function Reference() {
         <div className="flex flex-col justify-center items-center dark:bg-darksblue">
             <h1 className="text-4xl mb-3 text-darksblue dark:text-white">Referências do Projeto</h1>
             <div className={isMobile ? "grid grid-cols-1 w-64 h-auto" : "grid grid-cols-2 gap-4"}>
-                <img src={TestReference} alt="" />
-                <img src={TestReference} alt="" />
-                <img src={TestReference} alt="" />
-                <img src={TestReference} alt="" />
+                <a href="http://milanesa.ime.usp.br/rbie/index.php/rbie/article/view/7140" target="_blank" ><img src={TestReference} alt="" /></a>
+                <a href="https://www.scielo.br/j/rbee/a/n48zDNF4bSqzs7xHwQQ774c/" target="_blank" ><img src={TestReference} alt="" /></a>
+                <a href="https://seer.faccat.br/index.php/redin/article/view/1539" target="_blank" ><img src={TestReference} alt="" /></a>
+                <a href="http://milanesa.ime.usp.br/rbie/index.php/sbie/article/view/3057" target="_blank" ><img src={TestReference} alt="" /></a>
             </div>
         </div>
     )
